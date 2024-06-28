6757ba273ce9ade8e5f52380eb176e3379e88803
Yanako Lajara <127878396+yanakolajara@users.noreply.github.com>
2024-06-28 18:31:04 -0400

    refactoring: deleted summary component

--- src/routes/Home/Details.jsx
+++ src/routes/Home/Details.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Summary from './Details/Summary';
 import Education from './Details/Education';
 import JobCard from './Details/JobCard';
 import Selector from '../../components/Selector';
@@ -15,7 +14,24 @@ export default function Details() {
         setSelected={setSelected}
       />
 
-      {selected === 'summary' && <Summary />}
+      {selected === 'summary' && (
+        <div>
+          <p>
+            I am a full-stack developer at Pursuit, interested in creating new
+            technologies to make everyone's life more comfortable. Pursuit is an
+            intensive 12-month software engineering fellowship with a 9%
+            acceptance rate. I am skilled in Postgres, Express.js, React.js,
+            JavaScript, Java, Python, HTML, CSS, Node, Git, Algorithms, code
+            reviews, and pair programming. I started my developer journey in
+            2016 by learning basic HTML, CSS, JavaScript, and Arduino at a
+            summer club. Since that, my passion for technology and programming
+            has been growing exponentially. A fun fact about me that makes me an
+            enthusiastic developer is that even if coding is part of my
+            professional career, whenever I have free time, I still use it to
+            code for fun instead of taking a break.
+          </p>
+        </div>
+      )}
       {selected === 'education' && <Education />}
 
       {selected === 'job experience' && (
