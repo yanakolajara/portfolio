d17b04e23315e635fd9cb8b3ef0e543b9cda9cda
Yanako Lajara <127878396+yanakolajara@users.noreply.github.com>
2024-07-06 18:08:58 -0400

    feature: updated navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,19 @@ export default function Navbar() {
       <nav className='container-glass navbar'>
         <div className='portfolio-sections'>
           <a href='/'>Home</a>
-          <a href='/about-me'>More about me</a>
-          <a href='/favorite-resources'>Favorite resources</a>
+          {/* <a href='/about-me'>More about me</a> */}
+          {/* <a href='/favorite-resources'>Favorite resources</a> */}
         </div>
         <div className='contact-links'>
-          <a href='/'>GitHub</a>
-          <a href='/'>LinkedIn</a>
-          <a href='/'>Blog</a>
+          <a target='_blank' href='https://github.com/yanakolajara'>
+            GitHub
+          </a>
+          <a target='_blank' href='https://www.linkedin.com/in/yanakolajara/'>
+            LinkedIn
+          </a>
+          {/* <a target='_blank' href='/'>
+            Blog
+          </a> */}
           <a href='/'>Download CV</a>
         </div>
       </nav>
