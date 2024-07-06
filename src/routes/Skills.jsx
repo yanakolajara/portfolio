780001c40fa3e1be85d504910300bd1a2666f99c
Yanako Lajara <127878396+yanakolajara@users.noreply.github.com>
2024-07-06 18:54:36 -0400

    feature: skills selector

--- src/routes/Skills.jsx
+++ src/routes/Skills.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ToolCard from './Home/Skills/ToolCard';
+import Selector from '../components/Selector';
 
 export default function Skills() {
   const [selected, setSelected] = React.useState('All');
@@ -11,13 +12,12 @@ export default function Skills() {
 
   return (
     <div className='skills container-glass'>
-      <div className='selector'>
-        <button onclick={handleClick}>All</button>
-        <button onclick={handleClick}>Languages</button>
-        <button onclick={handleClick}>Frameworks</button>
-        <button onclick={handleClick}>Tools</button>
-        <button onclick={handleClick}>Other</button>
-      </div>
+      <Selector
+        options={['All', 'Languages', 'Frameworks', 'Tools', 'Other']}
+        selected={selected}
+        setSelected={setSelected}
+      />
+
       <p>Last updated: 01/01/2022</p>
       <div className='scroll-container container-deep'>
         <ToolCard />
