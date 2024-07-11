bfc8c999d2b391e9f90a121a11cda2a8ac5d4cc1
Yanako Lajara <127878396+yanakolajara@users.noreply.github.com>
2024-07-11 12:20:14 -0400

    Fix: education cards separated with line

--- src/routes/Home/Details.jsx
+++ src/routes/Home/Details.jsx
@@ -35,19 +35,22 @@ export default function Details() {
         <div className='education'>
           {educationData.map((education) => (
             <React.Fragment>
-              <div className='education__image'>
-                <img
-                  src={require(`../../assets/images/${education['image']}`)}
-                  alt={education.name}
-                />
+              <div className='education__card'>
+                <div className='education__image'>
+                  <img
+                    src={require(`../../assets/images/${education['image']}`)}
+                    alt={education.name}
+                  />
+                </div>
+                <div className='education__details'>
+                  <h1>{education.name}</h1>
+                  <p>Degree type: {education.degree}</p>
+                  <p>
+                    {education['start-date']} - {education['end-date']}
+                  </p>
+                </div>
               </div>
-              <div className='education__details'>
-                <h1>{education.name}</h1>
-                <p>Degree type: {education.degree}</p>
-                <p>
-                  {education['start-date']} - {education['end-date']}
-                </p>
-              </div>
+              <hr />
             </React.Fragment>
           ))}
         </div>
