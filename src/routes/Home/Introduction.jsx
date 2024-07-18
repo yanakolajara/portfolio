29cad08ee819707392976b05366af75559d456cd
Yanako Lajara <127878396+yanakolajara@users.noreply.github.com>
2024-07-18 00:41:04 -0400

    fix: introduction style

--- src/routes/Home/Introduction.jsx
+++ src/routes/Home/Introduction.jsx
@@ -3,16 +3,11 @@ import React from 'react';
 export default function Introduction({ profilePicture }) {
   return (
     <div className='introduction container-glass'>
-      <div className='profile-picture'>
-        <img
-          src={profilePicture}
-          alt='profile-pic'
-          className='profile-picture__img'
-        />
-      </div>
+      <img src={profilePicture} alt='profile-pic' className='profile-picture' />
       <div className='introduction-box'>
         <p className='name'>Yanako Lajara</p>
         <p className='title'>Full-Stack Web Developer</p>
+        <hr />
       </div>
 
       <div className='summary'>
